12a771da1bdb40dc90ba690ffcd126ba549bcbe6
HansThePanzergrenadier <49830583+HansThePanzergrenadier@users.noreply.github.com>
2021-03-16 23:52:13 +0200

    Lab3
    Uploading files

---
+++ Lab3/Program.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Lab3_Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = null;
+            bool isRunned = false;
+
+            Console.WriteLine("Press Enter to start...");
+
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.Enter:
+                        if (isRunned)
+                        {
+                            break;
+                        }
+                        Console.WriteLine();
+                        driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                        start(driver, "http://todomvc.com/examples/angularjs/");
+                        isRunned = true;
+                        Console.WriteLine();
+                        Console.WriteLine("Press Escape to exit...");
+                        break;
+
+                    case ConsoleKey.Escape:
+                        quit(driver);
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        static void start(IWebDriver driver, string url)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+
+            TodosPage site = new TodosPage(driver);
+
+            site.AddTodo("Practice guitar");
+            site.AddTodo("Do exercises");
+            site.AddTodo("Go outside");
+            site.AddTodo("Do Lab3 for SQ");
+            site.AddTodo("Sleep 8 hours");
+
+            site.DeleteTodo(3);
+
+            site.CheckTodo(1);
+            site.CheckTodo(2);
+
+            site.ShowActive();
+            site.ShowCompleted();
+            site.ClearCompleted();
+        }
+
+        static void quit(IWebDriver driver)
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ Lab3/TodosPageTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Lab3_Selenium
+{
+    class TodosPageTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Navigate().GoToUrl("http://todomvc.com/examples/angularjs/");
+
+            TodosPage site = new TodosPage(driver);
+
+            site.AddTodo("Practice guitar");
+            site.AddTodo("Do exercises");
+            site.AddTodo("Go outside");
+            site.AddTodo("Do Lab3 for SQ");
+            site.AddTodo("Sleep 8 hours");
+
+            site.DeleteTodo(3);
+
+            site.CheckTodo(1);
+            site.CheckTodo(2);
+
+            site.ShowActive();
+            site.ShowCompleted();
+            site.ClearCompleted();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
